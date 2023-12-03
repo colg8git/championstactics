7e90c762ae1c91843dac750452b966b56b2c420b
colg8 <129955515+colg8git@users.noreply.github.com>
2023-12-03 16:59:27 +0200

    pub

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    autoplay: {
+      delay: 3000,
+      pauseOnMouseEnter: true,
+    },
+
+    
+
+
+
+    effect: 'cards',
+    cardsEffect: {
+      slideShadows: true
+    },
+});
